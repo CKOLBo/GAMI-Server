74a81428654309b143478fa1b1f4fde4dfdbfa8f
박하민 <hamin090403@gmail.com>
2025-11-26 11:17:58 +0900

    add: 멤버 디테일 엔티티 추가

---
+++ src/main/java/com/team/cklob/gami/domain/member/entity/MemberDetail.java
@@ -0,0 +1,42 @@
+package com.team.cklob.gami.domain.member.entity;
+
+import com.team.cklob.gami.domain.member.entity.constant.Gender;
+import com.team.cklob.gami.domain.member.entity.constant.Major;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "member_detail")
+public class MemberDetail {
+
+    @Id
+    @Column(name = "member_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @MapsId
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "major", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Major major;
+
+    @Column(name = "generation",  nullable = false, length = 10)
+    private Integer generation;
+
+    @Builder
+    public MemberDetail(Member member, Gender gender, Major major, Integer generation) {
+        this.member = member;
+        this.gender = gender;
+        this.major = major;
+        this.generation = generation;
+    }
+}
