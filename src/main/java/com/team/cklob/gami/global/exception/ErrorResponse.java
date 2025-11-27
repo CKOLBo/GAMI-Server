65d028f52ea0faf4d5be1aa8ace24a7427a986c1
박하민 <hamin090403@gmail.com>
2025-11-27 23:23:46 +0900

    add: ErrorResponse 추가

---
+++ src/main/java/com/team/cklob/gami/global/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.team.cklob.gami.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private int code;
+    private String message;
+}
