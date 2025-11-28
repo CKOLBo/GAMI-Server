87a7e4bd46767e3323911cca6be075ec4b34ebb9
박하민 <hamin090403@gmail.com>
2025-11-29 02:18:29 +0900

    add: ErrorCode 추가

--- src/main/java/com/team/cklob/gami/global/exception/ErrorCode.java
+++ src/main/java/com/team/cklob/gami/global/exception/ErrorCode.java
@@ -7,7 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
+    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+
     NOT_FOUND_MEMBER_DETAIL(404, "회원 상세 정보를 찾을 수 없습니다.");
+
 
     private  final int code;
     private final String message;
