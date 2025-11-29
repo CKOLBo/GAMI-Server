d6110e5ee0eb76135c355bcf7d0b86c3c50ea519
hej090224 <fc49854985@gmail.com>
2025-11-29 21:32:17 +0900

    feat: 게시글 엔티티 구현 및 회원 연관관계 추가

--- src/main/java/com/team/cklob/gami/domain/post/entity/Post.java
+++ src/main/java/com/team/cklob/gami/domain/post/entity/Post.java
@@ -1,7 +1,11 @@
 package com.team.cklob.gami.domain.post.entity;
 
+import com.team.cklob.gami.domain.member.entity.Member;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -15,23 +19,45 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "post_id",  nullable = false)
+    @Column(name = "post_id", nullable = false)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
     @Column(name = "title", nullable = false, length = 30)
     private String title;
 
-    @Column(name = "content",  nullable = false)
+    @Column(name = "content", nullable = false)
     private String content;
 
-    @Column(name = "created_at",  nullable = false)
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "update_at",  nullable = false)
+    @Column(name = "update_at", nullable = false)
     private LocalDateTime updateAt;
 
-    @Column(name = "like", nullable = false)
+    @Column(name = "like_count", nullable = false)
     private Integer likeCount;
+
+    public static Post create(Member member, String title, String content) {
+        LocalDateTime now = LocalDateTime.now();
+        return Post.builder()
+                .member(member)
+                .title(title)
+                .content(content)
+                .createdAt(now)
+                .updateAt(now)
+                .likeCount(0)
+                .build();
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.updateAt = LocalDateTime.now();
+    }
 
     public void incrementLikeCount() {
         this.likeCount += 1;
