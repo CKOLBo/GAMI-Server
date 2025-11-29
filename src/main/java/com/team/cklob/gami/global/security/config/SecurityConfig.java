2a8a2739db30d70d27ca8bf62de048f3156885b5
박하민 <hamin090403@gmail.com>
2025-11-29 16:48:35 +0900

    update: 모든 경로 permitAll로 수정

--- src/main/java/com/team/cklob/gami/global/security/config/SecurityConfig.java
+++ src/main/java/com/team/cklob/gami/global/security/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import com.team.cklob.gami.global.security.jwt.TokenParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -49,6 +50,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                                .requestMatchers("/**").permitAll()
                                 .anyRequest().denyAll()
                 )
 
