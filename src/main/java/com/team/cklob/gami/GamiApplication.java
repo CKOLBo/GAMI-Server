9121d47c1d638f0961ab287f7a3ff134eeff8566
박하민 <hamin090403@gmail.com>
2025-11-29 15:10:22 +0900

    add: @ConfigurationPropertiesScan 추가

--- src/main/java/com/team/cklob/gami/GamiApplication.java
+++ src/main/java/com/team/cklob/gami/GamiApplication.java
@@ -2,8 +2,10 @@ package com.team.cklob.gami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GamiApplication {
 
 	public static void main(String[] args) {
