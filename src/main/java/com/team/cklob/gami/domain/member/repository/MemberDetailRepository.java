50bb434967bf776594dd07bd313aee43147f7ef7
박하민 <hamin090403@gmail.com>
2025-11-29 15:10:50 +0900

    add: MemberDetails -> MemberDetail로 수정

--- src/main/java/com/team/cklob/gami/domain/member/repository/MemberDetailRepository.java
+++ src/main/java/com/team/cklob/gami/domain/member/repository/MemberDetailRepository.java
@@ -1,10 +1,10 @@
 package com.team.cklob.gami.domain.member.repository;
 
-import com.team.cklob.gami.global.auth.MemberDetails;
+import com.team.cklob.gami.domain.member.entity.MemberDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberDetailRepository extends JpaRepository<MemberDetails, Long> {
-    Optional<MemberDetails> findById(Long id);
+public interface MemberDetailRepository extends JpaRepository<MemberDetail, Long> {
+    Optional<MemberDetail> findById(Long id);
 }
