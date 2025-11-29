31957fdb72ea37c8bcfcbf68f96aab40d1ab477f
박하민 <hamin090403@gmail.com>
2025-11-29 15:11:59 +0900

    update: NotFoundDetail -> NotFoundDetailException으로 수정

--- src/main/java/com/team/cklob/gami/domain/member/exception/NotFoundMemberDetailException.java
+++ src/main/java/com/team/cklob/gami/domain/member/exception/NotFoundMemberDetailException.java
@@ -3,8 +3,8 @@ package com.team.cklob.gami.domain.member.exception;
 import com.team.cklob.gami.global.exception.ErrorCode;
 import com.team.cklob.gami.global.exception.GlobalException;
 
-public class NotFoundMemberDetail extends GlobalException {
-    public NotFoundMemberDetail() {
+public class NotFoundMemberDetailException extends GlobalException {
+    public NotFoundMemberDetailException() {
         super(ErrorCode.NOT_FOUND_MEMBER_DETAIL);
     }
 }
