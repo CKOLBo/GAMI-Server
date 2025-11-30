5cde68da58e097b3807bf0210ac0e28bde20806e
박하민 <hamin090403@gmail.com>
2025-12-01 01:31:48 +0900

    refactor: code 삭제

--- src/main/java/com/team/cklob/gami/domain/auth/presentation/dto/request/ChangePasswordRequest.java
+++ src/main/java/com/team/cklob/gami/domain/auth/presentation/dto/request/ChangePasswordRequest.java
@@ -2,6 +2,5 @@ package com.team.cklob.gami.domain.auth.presentation.dto.request;
 
 public record ChangePasswordRequest(
         String email,
-        String code,
         String newPassword
 ) {}
