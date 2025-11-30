fc2a062260aaea905de2194c1e4065886d73084c
cfcromn <hamin090403@gmail.com>
2025-11-30 19:31:03 +0900

    add: SignInService 추가 및 구현

---
+++ src/main/java/com/team/cklob/gami/domain/auth/service/impl/SignInServiceImpl.java
@@ -0,0 +1,54 @@
+package com.team.cklob.gami.domain.auth.service.impl;
+
+import com.team.cklob.gami.domain.auth.entity.RefreshToken;
+import com.team.cklob.gami.domain.auth.exception.NotFoundUserException;
+import com.team.cklob.gami.domain.auth.exception.UnauthorizedException;
+import com.team.cklob.gami.domain.auth.presentation.dto.request.SignInRequest;
+import com.team.cklob.gami.domain.auth.presentation.dto.responnse.TokenResponse;
+import com.team.cklob.gami.domain.auth.repository.RefreshTokenRepository;
+import com.team.cklob.gami.domain.auth.service.SignInService;
+import com.team.cklob.gami.domain.member.entity.Member;
+import com.team.cklob.gami.domain.member.repository.MemberRepository;
+import com.team.cklob.gami.global.security.jwt.JwtProvider;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class SignInServiceImpl implements SignInService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    @Transactional
+    public TokenResponse execute(SignInRequest request) {
+        Member member = memberRepository.findByEmail(request.email())
+                .orElseThrow(NotFoundUserException::new);
+
+        if (!passwordEncoder.matches(request.password(), member.getPassword())) {
+            throw new UnauthorizedException();
+        }
+
+        String accessToken = jwtProvider.generateAccessToken(member.getEmail(), member.getRole());
+        String refreshToken = jwtProvider.generateRefreshToken(member.getEmail());
+
+        refreshTokenRepository.save(RefreshToken.builder()
+                .email(member.getEmail())
+                .token(refreshToken)
+                .build());
+
+        return new TokenResponse(
+                accessToken,
+                refreshToken,
+                LocalDateTime.now().plusSeconds(jwtProvider.getAccessTokenTime()),
+                LocalDateTime.now().plusSeconds(jwtProvider.getRefreshTokenTime())
+        );
+    }
+}
