86e766bfd7d3a3f35dac88d6dabc55b7aa707f94
cfcromn <hamin090403@gmail.com>
2025-11-30 19:29:17 +0900

    add: VerifyCodeService 추가 및 구현

---
+++ src/main/java/com/team/cklob/gami/domain/auth/service/impl/VerifyCodeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.team.cklob.gami.domain.auth.service.impl;
+
+import com.team.cklob.gami.domain.auth.exception.NotFoundVerifyCodeException;
+import com.team.cklob.gami.domain.auth.exception.NotMatchedCodeException;
+import com.team.cklob.gami.domain.auth.presentation.dto.request.VerifyCodeRequest;
+import com.team.cklob.gami.domain.auth.service.VerifyCodeService;
+import com.team.cklob.gami.global.redis.RedisUtil;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class VerifyCodeServiceImpl implements VerifyCodeService {
+
+    private final RedisUtil redisUtil;
+
+    private static final String EMAIL_AUTH_PREFIX = "auth:email:";
+    private static final String VERIFY_TIME_PREFIX = "auth:verify:time:";
+    private static final long EXPIRE_MINUTES = 30L;
+
+
+    @Override
+    @Transactional
+    public void execute(VerifyCodeRequest request) {
+        String key = EMAIL_AUTH_PREFIX +  request.email();
+
+        if (!redisUtil.hasKey(key)) {
+            throw new NotFoundVerifyCodeException();
+        }
+
+        if (!redisUtil.getValue(key).equals(request.code())) {
+            throw new NotMatchedCodeException();
+        }
+
+        String verifyKey = VERIFY_TIME_PREFIX + request.email();
+        redisUtil.setVerifyStatus(verifyKey, request.code(), EXPIRE_MINUTES);
+    }
+}
