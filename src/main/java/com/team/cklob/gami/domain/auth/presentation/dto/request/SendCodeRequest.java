e37ab5c2ca0b212232cc12ff5823197cf1851a65
박하민 <hamin090403@gmail.com>
2025-11-30 23:11:41 +0900

    add: VerificationType 추가

--- src/main/java/com/team/cklob/gami/domain/auth/presentation/dto/request/SendCodeRequest.java
+++ src/main/java/com/team/cklob/gami/domain/auth/presentation/dto/request/SendCodeRequest.java
@@ -1,5 +1,8 @@
 package com.team.cklob.gami.domain.auth.presentation.dto.request;
 
+import com.team.cklob.gami.domain.auth.entity.constant.VerificationType;
+
 public record SendCodeRequest(
-        String email
+        String email,
+        VerificationType verificationType
 ) {}
