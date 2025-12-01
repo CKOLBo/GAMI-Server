54bd3f2180049322f0dcfc6e5837807480f79e77
박하민 <hamin090403@gmail.com>
2025-12-01 22:23:03 +0900

    refactor: 잘못된 import 수정

--- src/main/java/com/team/cklob/gami/domain/member/repository/MemberDetailRepository.java
+++ src/main/java/com/team/cklob/gami/domain/member/repository/MemberDetailRepository.java
@@ -2,7 +2,7 @@ package com.team.cklob.gami.domain.member.repository;
 
 import com.team.cklob.gami.domain.member.entity.MemberDetail;
 import com.team.cklob.gami.domain.member.entity.constant.Major;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
