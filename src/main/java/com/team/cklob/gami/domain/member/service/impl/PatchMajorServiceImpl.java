d00236b74ccc5126467925399641df54e7e3122f
박하민 <hamin090403@gmail.com>
2025-12-01 20:49:15 +0900

    add: PatchMajorServiceImpl 추가

---
+++ src/main/java/com/team/cklob/gami/domain/member/service/impl/PatchMajorServiceImpl.java
@@ -0,0 +1,32 @@
+package com.team.cklob.gami.domain.member.service.impl;
+
+import com.team.cklob.gami.domain.member.entity.Member;
+import com.team.cklob.gami.domain.member.entity.MemberDetail;
+import com.team.cklob.gami.domain.member.exception.NotFoundMemberDetailException;
+import com.team.cklob.gami.domain.member.presentation.dto.request.PatchMajorRequest;
+import com.team.cklob.gami.domain.member.repository.MemberDetailRepository;
+import com.team.cklob.gami.domain.member.service.PatchMajorService;
+import com.team.cklob.gami.global.util.MemberUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PatchMajorServiceImpl implements PatchMajorService {
+
+    private final MemberUtil memberUtil;
+    private final MemberDetailRepository memberDetailRepository;
+
+    @Override
+    @Transactional
+    public void execute(PatchMajorRequest request) {
+        Member member = memberUtil.getCurrentMember();
+
+        MemberDetail memberDetail = memberDetailRepository.findById(member.getId())
+                .orElseThrow(NotFoundMemberDetailException::new);
+
+        memberDetail.updateMajor(request.major());
+        memberDetailRepository.save(memberDetail);
+    }
+}
