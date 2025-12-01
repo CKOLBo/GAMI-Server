e75530b778166392d65c4c5fb335a2a587cb3ff9
박하민 <hamin090403@gmail.com>
2025-12-01 20:48:05 +0900

    add: GetMyProfileServiceImpl 추가

---
+++ src/main/java/com/team/cklob/gami/domain/member/service/impl/GetMyProfileServiceImpl.java
@@ -0,0 +1,37 @@
+package com.team.cklob.gami.domain.member.service.impl;
+
+import com.team.cklob.gami.domain.member.entity.Member;
+import com.team.cklob.gami.domain.member.entity.MemberDetail;
+import com.team.cklob.gami.domain.member.exception.NotFoundMemberDetailException;
+import com.team.cklob.gami.domain.member.presentation.dto.response.GetMemberProfileResponse;
+import com.team.cklob.gami.domain.member.repository.MemberDetailRepository;
+import com.team.cklob.gami.domain.member.service.GetMyProfileService;
+import com.team.cklob.gami.global.util.MemberUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GetMyProfileServiceImpl implements GetMyProfileService {
+
+    private final MemberDetailRepository  memberDetailRepository;
+    private final MemberUtil memberUtil;
+
+    @Override
+    @Transactional(readOnly = true)
+    public GetMemberProfileResponse execute() {
+        Member member = memberUtil.getCurrentMember();
+
+        MemberDetail memberDetail = memberDetailRepository.findById(member.getId())
+                .orElseThrow(NotFoundMemberDetailException::new);
+
+        return new GetMemberProfileResponse(
+                member.getId(),
+                member.getName(),
+                memberDetail.getGender(),
+                memberDetail.getGeneration(),
+                memberDetail.getMajor()
+        );
+    }
+}
