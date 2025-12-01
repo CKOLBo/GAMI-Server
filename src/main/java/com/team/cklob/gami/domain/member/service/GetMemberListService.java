de8ba5fa1174ab0af2252dd1c4b23f2704115d48
박하민 <hamin090403@gmail.com>
2025-12-01 20:46:42 +0900

    add: GetMemberListService 추가

---
+++ src/main/java/com/team/cklob/gami/domain/member/service/GetMemberListService.java
@@ -0,0 +1,16 @@
+package com.team.cklob.gami.domain.member.service;
+
+import com.team.cklob.gami.domain.member.entity.constant.Major;
+import com.team.cklob.gami.domain.member.presentation.dto.response.GetMemberProfileResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+
+public interface GetMemberListService {
+    Page<GetMemberProfileResponse> execute(
+            Major major,
+            String name,
+            Integer generation,
+            Pageable pageable
+    );
+}
