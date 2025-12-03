e2cf2b37253149db7f20cb217e8802849de8f523
박하민 <hamin090403@gmail.com>
2025-12-03 19:48:31 +0900

    add:  findAllByMenteeIdAndApplyStatusOrderByCreatedAtDesc, findAllByMentorIdAndApplyStatusOrderByCreatedAtDesc 추가

--- src/main/java/com/team/cklob/gami/domain/mentoring/repository/ApplyRepository.java
+++ src/main/java/com/team/cklob/gami/domain/mentoring/repository/ApplyRepository.java
@@ -1,9 +1,15 @@
 package com.team.cklob.gami.domain.mentoring.repository;
 
 import com.team.cklob.gami.domain.mentoring.entity.Apply;
+import com.team.cklob.gami.domain.mentoring.entity.constant.ApplyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
 
-    Boolean existsByMenteeIdAndMentorId(Long menteeId, Long mentorId);
+    Boolean existsByMenteeIdAndMentorIdAndApplyStatusIn(Long menteeId, Long mentorId, List<ApplyStatus> applyStatus);
+
+    List<Apply> findAllByMenteeIdAndApplyStatusOrderByCreatedAtDesc(Long menteeId, ApplyStatus applyStatus);
+    List<Apply> findAllByMentorIdAndApplyStatusOrderByCreatedAtDesc(Long mentorId, ApplyStatus applyStatus);
 }
