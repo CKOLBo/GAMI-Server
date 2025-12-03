500fa200e79cb1da7ec31770c50c44a69d8c3e7f
박하민 <hamin090403@gmail.com>
2025-12-03 20:16:45 +0900

    refactor: 불필요한 생성자 제거

--- src/main/java/com/team/cklob/gami/global/event/CreateChatRoomEvent.java
+++ src/main/java/com/team/cklob/gami/global/event/CreateChatRoomEvent.java
@@ -4,10 +4,4 @@ public record CreateChatRoomEvent(
    Long menteeId,
    Long mentorId,
    Long applyId
-) {
-    public CreateChatRoomEvent(Long menteeId, Long mentorId, Long applyId) {
-        this.menteeId = menteeId;
-        this.mentorId = mentorId;
-        this.applyId = applyId;
-    }
-}
+) {}
