24f03740affe8164d7645be19bcd7ae579ac135e
박하민 <hamin090403@gmail.com>
2025-12-08 17:30:09 +0900

    add: ChatSystemMessageResponse 추가

---
+++ src/main/java/com/team/cklob/gami/domain/chat/presentation/response/ChatSystemMessageResponse.java
@@ -0,0 +1,14 @@
+package com.team.cklob.gami.domain.chat.presentation.response;
+
+import com.team.cklob.gami.domain.chat.entity.constant.MessageType;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record ChatSystemMessageResponse(
+        MessageType type,
+        String content,
+        LocalDateTime timestamp
+) {
+}
