e4d773ab8483c2219afde499146e91e0b9ae6baa
hej090224 <fc49854985@gmail.com>
2025-12-09 08:48:28 +0900

    repactor: 코드 스타일 통일

--- src/main/java/com/team/cklob/gami/global/exception/ErrorCode.java
+++ src/main/java/com/team/cklob/gami/global/exception/ErrorCode.java
@@ -10,6 +10,13 @@ public enum ErrorCode {
     // AUTH
     EXPIRED_TOKEN(401, "토큰이 만료되었습니다."),
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
+    UNVERIFIED_EMAIL(400, "인증되지 않은 이메일입니다."),
+    NOT_MATCHED_CODE(400, "인증 코드가 일치하지 않습니다"),
+    NOT_FOUND_USER(401, "존재하지 않는 사용자입니다."),
+    UNAUTHORIZED(401, "이메일 또는 비밀번호가 잘못되었습니다."),
+    NOT_FOUND_VERIFY_CODE(404, "만료되었거나 존재하지 않는 인증 코드입니다."),
+    TOO_MANY_REQUESTS(429, "너무 빠르게 요청하고 있습니다. 잠시 후 다시 시도해주세요."),
+    EMAIL_ALREADY_EXISTS(409, "이미 등록된 이메일입니다"),
 
     // COMMON
     INTERNAL_SERVER_ERROR(500, "예기치 못한 서버 에러가 발생했습니다."),
@@ -19,15 +26,6 @@ public enum ErrorCode {
     INVALID_MEMBER_PRINCIPAL(401, "현재 인증된 사용자의 정보가 유효하지 않습니다."),
     NOT_FOUND_MEMBER_DETAIL(404, "회원 상세 정보를 찾을 수 없습니다."),
     NOT_FOUND_MEMBER(404, "해당 회원을 찾을 수 없습니다."),
-
-    //auth
-    UNVERIFIED_EMAIL(400, "인증되지 않은 이메일입니다."),
-    NOT_MATCHED_CODE(400, "인증 코드가 일치하지 않습니다"),
-    NOT_FOUND_USER(401, "존재하지 않는 사용자입니다."),
-    UNAUTHORIZED(401, "이메일 또는 비밀번호가 잘못되었습니다."),
-    NOT_FOUND_VERIFY_CODE(404, "만료되었거나 존재하지 않는 인증 코드입니다."),
-    TOO_MANY_REQUESTS(429, "너무 빠르게 요청하고 있습니다. 잠시 후 다시 시도해주세요."),
-    EMAIL_ALREADY_EXISTS(409, "이미 등록된 이메일입니다"),
   
     // POST
     INVALID_POST_REQUEST(400, "게시글 요청 정보가 유효하지 않습니다."),
@@ -40,13 +38,13 @@ public enum ErrorCode {
     NOT_FOUND_COMMENT(404, "해당 댓글을 찾을 수 없습니다."),
     FORBIDDEN_COMMENT_ACCESS(403, "댓글에 대한 권한이 없습니다."),
 
-    // mentoring
+    // MENTORING
     RANDOM_MENTOR_NOT_FOUND(404, "조건에 맞는 멘토를 찾을 수 없습니다."),
     ALREADY_REGISTERED_MENTOR(409, "이미 등록된 멘토입니다"),
     APPLY_NOT_FOUND(404, "존재하지 않는 신청입니다."),
     SELF_APPLY_NOT_ALLOWED(400, "자기 자신한테 신청을 보낼 수 없습니다."),
 
-    // chat
+    // CHAT
     ALREADY_EXIST_CHAT_ROOM(400, "이미 생성된 채팅방입니다."),
     ALREADY_LEFT_CHAT_ROOM(409, "이미 나간 채팅방입니다."),
     NOT_CHAT_ROOM_MEMBER(403, "채팅방 멤버가 아닙니다."),
