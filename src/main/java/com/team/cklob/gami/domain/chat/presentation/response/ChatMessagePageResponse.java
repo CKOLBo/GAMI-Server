6d9984eefb120b14936c02f5480e196ec790fcdf
박하민 <hamin090403@gmail.com>
2025-12-08 17:29:22 +0900

    add: ChatMessagePageResponse 추가

---
+++ src/main/java/com/team/cklob/gami/domain/chat/presentation/response/ChatMessagePageResponse.java
@@ -0,0 +1,17 @@
+package com.team.cklob.gami.domain.chat.presentation.response;
+
+import com.team.cklob.gami.domain.chat.entity.constant.RoomStatus;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record ChatMessagePageResponse(
+        Long roomId,
+        List<ChatMessageResponse> messages,
+        Long nextCursor,
+        boolean hasMore,
+        RoomStatus roomStatus,
+        boolean currentMemberLeft
+) {
+}
