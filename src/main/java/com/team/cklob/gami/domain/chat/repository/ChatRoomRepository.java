48722c103f2296eb5f1cb3bc78aeefa71da8451b
박하민 <hamin090403@gmail.com>
2025-12-08 17:33:53 +0900

    update: findOtherMemberInRoom으로 변경 및 findAllByMentorIdOrMenteeId 추가

--- src/main/java/com/team/cklob/gami/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/team/cklob/gami/domain/chat/repository/ChatRoomRepository.java
@@ -1,12 +1,31 @@
 package com.team.cklob.gami.domain.chat.repository;
 
 import com.team.cklob.gami.domain.chat.entity.ChatRoom;
+import com.team.cklob.gami.domain.member.entity.MemberDetail;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     boolean existsByMenteeIdAndMentorId(Long menteeId, Long mentorId);
 
-    List<ChatRoom> findByMenteeIdAndMentorId(Long menteeId, Long mentorId);
+    @Query("SELECT CASE WHEN COUNT(cr) > 0 THEN true ELSE false END FROM ChatRoom cr " +
+            "WHERE cr.id = :roomId AND (cr.mentor.id = :memberId OR cr.mentee.id = :memberId)")
+    boolean existsByRoomIdAndMemberId(@Param("memberId") Long memberId, @Param("roomId") Long roomId);
+
+    @Query("""
+    select md
+    from ChatRoom cr
+    join MemberDetail md
+      on (cr.mentor.id = :memberId and md.member = cr.mentee)
+      or (cr.mentee.id = :memberId and md.member = cr.mentor)
+    where cr.id = :roomId
+    """)
+    Optional<MemberDetail> findOtherMemberInRoom(@Param("roomId") Long roomId,
+                                                 @Param("memberId") Long memberId);
+
+    List<ChatRoom> findAllByMentorIdOrMenteeId(Long mentorId, Long menteeId);
 }
