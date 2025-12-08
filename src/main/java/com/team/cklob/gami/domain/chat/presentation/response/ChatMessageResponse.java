1cc9b82c6edc836d2f9e90c0481aae3e4cccaa5b
박하민 <hamin090403@gmail.com>
2025-12-08 17:29:54 +0900

    add: from 추가

--- src/main/java/com/team/cklob/gami/domain/chat/presentation/response/ChatMessageResponse.java
+++ src/main/java/com/team/cklob/gami/domain/chat/presentation/response/ChatMessageResponse.java
@@ -1,11 +1,23 @@
 package com.team.cklob.gami.domain.chat.presentation.response;
 
+import com.team.cklob.gami.domain.chat.entity.ChatMessage;
+
 import java.time.LocalDateTime;
 
 public record ChatMessageResponse(
         Long messageId,
-        Long roomId,
         String message,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        Long senderId,
+        String senderName
 ) {
+    public static ChatMessageResponse from(ChatMessage message, Long currentUserId) {
+        return new ChatMessageResponse(
+                message.getId(),
+                message.getMessage(),
+                message.getCreatedAt(),
+                message.getSender().getId(),
+                message.getSender().getName()
+        );
+    }
 }
