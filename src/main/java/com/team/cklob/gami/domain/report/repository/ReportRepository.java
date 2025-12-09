039e39b411d5a91267b098b62c7410c5fe6a74c4
hej090224 <fc49854985@gmail.com>
2025-12-09 20:23:51 +0900

    feat(report): 동일 게시글 중복 신고 여부 확인 추가

--- src/main/java/com/team/cklob/gami/domain/report/repository/ReportRepository.java
+++ src/main/java/com/team/cklob/gami/domain/report/repository/ReportRepository.java
@@ -1,8 +1,10 @@
 package com.team.cklob.gami.domain.report.repository;
 
 import com.team.cklob.gami.domain.report.entity.Report;
+import com.team.cklob.gami.domain.report.entity.constant.ReportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
-    boolean existsByReporterIdAndPostId(Long reporterId, Long postId);
+
+    boolean existsByReporterIdAndPostIdAndReportType(Long reporterId, Long postId, ReportType reportType);
 }
