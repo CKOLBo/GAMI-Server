64ef3df3c87652dd9cc288f43153af6bfe0c0679
hej090224 <fc49854985@gmail.com>
2025-12-12 10:52:24 +0900

    feat: 게시글 이미지 JPA Repository 추가

---
+++ src/main/java/com/team/cklob/gami/domain/post/repository/PostImageRepository.java
@@ -0,0 +1,10 @@
+package com.team.cklob.gami.domain.post.repository;
+
+import com.team.cklob.gami.domain.post.entity.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+    List<PostImage> findAllByPostIdOrderBySequenceAsc(Long postId);
+}
