253953dac5e0b7193b439a9a5c6eec3004cdcd66
hej090224 <fc49854985@gmail.com>
2025-12-12 10:53:46 +0900

    feat: 게시글 생성 시 이미지 목록 저장 인터페이스, 구현체 구현

--- src/main/java/com/team/cklob/gami/domain/post/service/PostCreateService.java
+++ src/main/java/com/team/cklob/gami/domain/post/service/PostCreateService.java
@@ -3,6 +3,5 @@ package com.team.cklob.gami.domain.post.service;
 import com.team.cklob.gami.domain.post.dto.request.PostCreateRequest;
 
 public interface PostCreateService {
-
-    Long create(PostCreateRequest request);
+    Long create(PostCreateRequest request, Long memberId);
 }
