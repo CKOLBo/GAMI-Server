e950094e7424a3531c6da100151d155353bb4d3a
박하민 <hamin090403@gmail.com>
2025-12-14 23:00:47 +0900

    add :: PostSummaryResponse 추가

--- src/main/java/com/team/cklob/gami/domain/post/dto/response/PostSummaryResponse.java
+++ src/main/java/com/team/cklob/gami/domain/post/dto/response/PostSummaryResponse.java
@@ -1,30 +1,15 @@
 package com.team.cklob.gami.domain.post.dto.response;
 
-import com.team.cklob.gami.domain.post.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PostSummaryResponse {
-
-    private Long id;
-
-    private String title;
-
-    private LocalDateTime createdAt;
-
-    public static PostSummaryResponse from(Post post) {
-        return PostSummaryResponse.builder()
-                .id(post.getId())
-                .title(post.getTitle())
-                .createdAt(post.getCreatedAt())
-                .build();
-    }
+    private Long postId;
+    private String summary;
 }
