7876154274ae1512674d31ee6a91574950942411
박하민 <hamin090403@gmail.com>
2025-12-14 23:04:46 +0900

    add :: summary 추가

--- src/main/java/com/team/cklob/gami/domain/post/entity/Post.java
+++ src/main/java/com/team/cklob/gami/domain/post/entity/Post.java
@@ -59,6 +59,7 @@ public class Post {
                 .member(member)
                 .title(title)
                 .content(content)
+                .summary("")
                 .build();
     }
 
