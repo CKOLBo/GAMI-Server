1355380de994b8ff3afc5df4b154156a97b252a1
cfcromn <hamin090403@gmail.com>
2025-12-14 19:14:26 +0900

    add :: AiController 추가

---
+++ src/main/java/com/team/cklob/gami/domain/ai/controller/AiController.java
@@ -0,0 +1,10 @@
+package com.team.cklob.gami.domain.ai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AiController {
+
+}
