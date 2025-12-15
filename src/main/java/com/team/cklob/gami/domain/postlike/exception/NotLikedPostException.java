3b20da3a558a1efec259ef172179fb71ef66303c
hej090224 <fc49854985@gmail.com>
2025-12-15 11:27:15 +0900

    feat: 좋아요 하지 않은 게시글 예외 추가

---
+++ src/main/java/com/team/cklob/gami/domain/postlike/exception/NotLikedPostException.java
@@ -0,0 +1,10 @@
+package com.team.cklob.gami.domain.postlike.exception;
+
+import com.team.cklob.gami.global.exception.ErrorCode;
+import com.team.cklob.gami.global.exception.GlobalException;
+
+public class NotLikedPostException extends GlobalException {
+    public NotLikedPostException() {
+        super(ErrorCode.NOT_LIKED_POST);
+    }
+}
