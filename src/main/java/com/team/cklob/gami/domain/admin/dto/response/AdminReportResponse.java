776a37c5abaddb8e5da2c657a5c4639504d7b6b8
hej090224 <fc49854985@gmail.com>
2025-12-27 21:56:03 +0900

    feat: 어드민 신고 목록 조회 응답 필드 추가

--- src/main/java/com/team/cklob/gami/domain/admin/dto/response/AdminReportResponse.java
+++ src/main/java/com/team/cklob/gami/domain/admin/dto/response/AdminReportResponse.java
@@ -1,6 +1,7 @@
 package com.team.cklob.gami.domain.admin.dto.response;
 
 import com.team.cklob.gami.domain.report.entity.Report;
+import com.team.cklob.gami.domain.report.entity.constant.ReportResult;
 import com.team.cklob.gami.domain.report.entity.constant.ReportType;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,11 +12,17 @@ public class AdminReportResponse {
 
     private final Long id;
     private final ReportType reportType;
+    private final ReportResult reportResult;
+    private final String reporterName;
+    private final Long reportedPostId;
 
     public static AdminReportResponse from(Report report) {
         return AdminReportResponse.builder()
                 .id(report.getId())
                 .reportType(report.getReportType())
+                .reportResult(report.getResult())
+                .reporterName(report.getReporter().getName())
+                .reportedPostId(report.getPost().getId())
                 .build();
     }
 }
