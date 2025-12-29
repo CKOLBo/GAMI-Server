766ee73e70f227579ede25324a6d5a72d01d0429
박하민 <hamin090403@gmail.com>
2025-12-29 18:31:13 +0900

    add :: memberUtil 추가

--- src/main/java/com/team/cklob/gami/domain/mentoring/service/impl/GetMentorListServiceImpl.java
+++ src/main/java/com/team/cklob/gami/domain/mentoring/service/impl/GetMentorListServiceImpl.java
@@ -1,11 +1,13 @@
 package com.team.cklob.gami.domain.mentoring.service.impl;
 
+import com.team.cklob.gami.domain.chat.entity.constant.RoomStatus;
 import com.team.cklob.gami.domain.member.entity.Member;
 import com.team.cklob.gami.domain.member.entity.MemberDetail;
 import com.team.cklob.gami.domain.member.entity.constant.Major;
 import com.team.cklob.gami.domain.member.repository.MemberDetailRepository;
 import com.team.cklob.gami.domain.mentoring.dto.response.GetMentorResponse;
 import com.team.cklob.gami.domain.mentoring.service.GetMentorListService;
+import com.team.cklob.gami.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class GetMentorListServiceImpl implements GetMentorListService {
 
     private final MemberDetailRepository memberDetailRepository;
+    private final MemberUtil memberUtil;
 
     @Override
     @Transactional(readOnly = true)
@@ -25,9 +28,10 @@ public class GetMentorListServiceImpl implements GetMentorListService {
             String name,
             Integer generation,
             Pageable pageable) {
+        Member member = memberUtil.getCurrentMember();
 
-        Page<MemberDetail> detailsPage = memberDetailRepository.findAllWithFiltersIncludingSeniors(
-                major, name, generation, pageable
+        Page<MemberDetail> detailsPage = memberDetailRepository.findAllWithFiltersIncludingSeniorsExcludeActiveRoom(
+                major, name, generation, member.getId(), RoomStatus.ACTIVE, pageable
         );
 
         return detailsPage.map(detail -> {
